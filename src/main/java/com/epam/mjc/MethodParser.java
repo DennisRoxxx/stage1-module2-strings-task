89b35ee75dd4073cd4efcc4fea1755ef4aee7648
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-23 12:33:59 +0300

    Update MethodParser.java

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,9 +1,7 @@
 package com.epam.mjc;
 
 public class MethodParser {
-    private static final String REG_EXP_DELIMITER = " *\\( *|\\)|( *,+ +)| +";
-    private final List<String> accessModifiers = List.of("public", "private", "protected");
-
+    
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
@@ -22,24 +20,32 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        String modifier;
-        String type;
-        String name;
-        String[] signatureElements = signatureString.trim().split(REG_EXP_DELIMITER);
-        modifier = isAccessModifier(signatureElements[0]) ? signatureElements[0] : null;
-        type = modifier == null ? signatureElements[0] : signatureElements[1];
-        name = modifier == null ? signatureElements[1] : signatureElements[2];
-        int index = modifier == null ? 2 : 3;
-        List<MethodSignature.Argument> arguments = new ArrayList<>();
-        for (int i = index; i < signatureElements.length; i += 2) {
-            arguments.add(new MethodSignature.Argument(signatureElements[i], signatureElements[i+1]));
+        
+        StringTokenizer stz = new StringTokenizer(signatureString, "()");
+        
+        ArrayList<MethodSignature.Argument> argList = new ArrayList<>();
+        
+        String tempArgs = stz.nextToken();
+        
+        String[] textArgs = tempArgs.split(" ");
+        
+        if (stz.hasMoreTokens()) {
+            String arguments = stz.nextToken();
+            String[] args = arguments.split(", ");
+            
+            for (String item : args) {
+                String[] items = item.split(" ");
+                argList.add(new MethodSignature.Argument(items[0], items[1]));
+            }
         }
-        MethodSignature methodSignature = new MethodSignature(name, arguments);
-        methodSignature.setAccessModifier(modifier);
-        methodSignature.setReturnType(type);
-        return methodSignature;
-    }
-    private boolean isAccessModifier(String modifier){
-        return accessModifiers.contains(modifier);
+
+        MethodSignature signature = new MethodSignature(textArgs[textArgs.length - 1], argList);
+        if (textArgs.length == 2) {
+            signature.setReturnType(prefixes[0]);
+        } else {
+            signature.setAccessModifier(prefixes[0]);
+            signature.setReturnType(prefixes[1]);
+        }
+        return signature;
     }
 }
